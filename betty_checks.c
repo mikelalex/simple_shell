76fbe71cbe5d0569270289d9cb7947e609400304
Jaybest01 <judestephen108@gmail.com>
2023-04-16 09:01:13 -0700

    0. Betty would be proud

---
+++ betty_checks.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This program prints "command line code that passes the Betty checks" to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        printf("Usage: %s <argument>\n", argv[0]);
+        return 1;
+    }
+
+    printf("You entered: %s\n", argv[1]);
+    return 0;
+}
+
