3dee4bed2e167335432351d93956c761f35f814e
Jaybest01 <judestephen108@gmail.com>
2023-04-16 17:14:30 -0700

    6. Simple shell 0.1.1

---
+++ getline.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+char* my_getline() {
+    static char buffer[BUFFER_SIZE];
+    static int pos = 0;
+    static ssize_t len = 0;
+    char* line = NULL;
+    int line_len = 0;
+    int c;
+
+    while (1) {
+        if (pos >= len) {
+            len = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+            if (len <= 0) {
+                break;
+            }
+            pos = 0;
+        }
+
+        c = buffer[pos++];
+        if (c == '\n' || c == EOF) {
+            break;
+        }
+
+        if (line_len % BUFFER_SIZE == 0) {
+            line = realloc(line, line_len + BUFFER_SIZE);
+            if (!line) {
+                perror("realloc");
+                exit(EXIT_FAILURE);
+            }
+        }
+        line[line_len++] = c;
+    }
+
+    if (line_len == 0 && len <= 0) {
+        return NULL;
+    }
+
+    if (line_len % BUFFER_SIZE == 0) {
+        line = realloc(line, line_len + 1);
+        if (!line) {
+            perror("realloc");
+            exit(EXIT_FAILURE);
+        }
+    }
+    line[line_len] = '\0';
+
+    return line;
+}
+
+int main() {
+    char* line;
+
+    while (1) {
+        printf("> ");
+        fflush(stdout);
+
+        line = my_getline();
+        if (line == NULL) {
+            printf("Goodbye!\n");
+            break;
+        }
+
+        printf("You typed: %s\n", line);
+        free(line);
+    }
+
+    return 0;
+}
+
