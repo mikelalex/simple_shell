c25601aa7be718552ed2f4d684057f229c8100cf
Jaybest01 <104685590+Jaybest01@users.noreply.github.com>
2023-04-21 21:42:39 +0100

    Update shell1.c

--- shell1.c
+++ shell1.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-
-#define MAX_COMMAND_LENGTH 1024
-#define PROMPT "> "
+#include <shell.h>
+/*
+*
+*Handle command lines with arguments
+*
+*/
 
 int main(void)
 {
