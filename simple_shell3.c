b0e85e89bdc4d3636a666bb221a2321faf949b82
Jaybest01 <judestephen108@gmail.com>
2023-04-21 16:31:12 -0700

    simple_shell3.c

---
+++ simple_shell3.c
@@ -0,0 +1,102 @@
+#include "shell.h"
+
+/*
+*Simple shell 0.3 +
+*Implement the exit built-in, that exits the shell
+*Usage: exit
+*You don’t have to handle any argument to the built-in exit
+*
+*/
+
+int main(void)
+{
+    char command[MAX_COMMAND_LENGTH];
+    char *args[MAX_COMMAND_LENGTH];
+    char command_path[MAX_COMMAND_LENGTH];
+    char *path;
+    char *token;
+    int status;
+    pid_t pid;
+    int found;
+    int i;
+
+    while (1)
+    {
+        printf(PROMPT);
+
+        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+        {
+            printf("\n");
+            exit(EXIT_SUCCESS);
+        }
+
+        command[strcspn(command, "\n")] = '\0';
+
+        i = 0;
+        token = strtok(command, " ");
+
+        while (token != NULL) 
+        {
+            args[i] = token;
+            i++;
+            token = strtok(NULL, " ");
+        }
+
+        args[i] = NULL;
+
+        /* Handle the exit built-in command */
+        if (strcmp(args[0], "exit") == 0)
+        {
+            exit(EXIT_SUCCESS);
+        }
+
+        /* Check if the command exists */
+        found = 0;
+        path = getenv("PATH");
+        token = strtok(path, ":");
+        while (token != NULL)
+        {
+            sprintf(command_path, "%s/%s", token, args[0]);
+            if (access(command_path, F_OK) == 0)
+            {
+                found = 1;
+                break;
+            }
+            token = strtok(NULL, ":");
+        }
+
+        if (!found)
+        {
+            printf("%s: command not found\n", args[0]);
+            continue;
+        }
+
+        pid = fork();
+
+        if (pid == -1)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0)
+        {
+            if (execv(command_path, args) == -1)
+            {
+                perror(args[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            if (wait(&status) == -1)
+            {
+                perror("wait");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    return 0;
+}
+
