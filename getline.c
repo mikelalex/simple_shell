ffde7866280601604c498bcb2ebdbdc6f3f07d70
Jaybest01 <104685590+Jaybest01@users.noreply.github.com>
2023-04-21 21:52:20 +0100

    Update getline.c

--- getline.c
+++ getline.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <shell.h>
 
-#define BUFFER_SIZE 1024
+/*
+*
+*Write your own getline function
+*Use a buffer to read many chars at once and call the least possible the read system call
+*You will need to use static variables
+*You are not allowed to use getline
+*
+*
 
 char* my_getline() {
     static char buffer[BUFFER_SIZE];
